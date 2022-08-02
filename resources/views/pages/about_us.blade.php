bad6ceda5b3a401c8e74a7ddf3173daaea4df758
Andrej Kopp <admin@sequel.one>
2022-08-02 20:35:05 +0200

    Fixed fakes meta tags in templates

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.app')
 
 @section('head')
-    <title>{{ !empty($extras->meta_title) ? $extras->meta_title : '' }} | {{ config('app.name', 'Laravel') }}</title>
+    <title>{{ !empty($page->meta_title) ? $page->meta_title : '' }} | {{ config('app.name', 'Laravel') }}</title>
     <meta name="twitter:card" content="summary">
     <meta name="twitter:site" content="@sequeloneinc">
     <meta name="twitter:creator" content="@sequeloneinc">
-    <meta name="twitter:title" content="{{ !empty($extras->meta_title) ? $extras->meta_title : '' }} | {{ config('app.name', 'Laravel') }}">
-    <meta name="twitter:description" content="{{ !empty($extras->meta_description) ? $extras->meta_description : '' }}">
+    <meta name="twitter:title" content="{{ !empty($page->meta_title) ? $page->meta_title : '' }} | {{ config('app.name', 'Laravel') }}">
+    <meta name="twitter:description" content="{{ !empty($page->meta_description) ? $page->meta_description : '' }}">
     <meta name="twitter:image" content="{{ asset('img/logo.png') }}">
 
     <!-- Facebook -->
     <meta property="og:url" content="{{ url()->current() }}">
-    <meta property="og:title" content="{{ !empty($extras->meta_title) ? $extras->meta_title : '' }} | {{ config('app.name', 'Laravel') }}">
-    <meta property="og:description" name="description" content="{{ !empty($extras->meta_description) ? $extras->meta_description : '' }}">
+    <meta property="og:title" content="{{ !empty($page->meta_title) ? $page->meta_title : '' }} | {{ config('app.name', 'Laravel') }}">
+    <meta property="og:description" name="description" content="{{ !empty($page->meta_description) ? $page->meta_description : '' }}">
     <meta property="og:type" content="website">
     <meta property="og:image" content="{{ asset('img/logo.png') }}">
     <meta property="og:image:secure_url" content="{{ asset('img/logo.png') }}">
     <meta property="og:image:type" content="image/png">
     <meta property="og:image:width" content="1200">
     <meta property="og:image:height" content="630">
-    <meta name="keywords" content="{{ !empty($extras->meta_keywords) ? $extras->meta_keywords : '' }}">
+    <meta name="keywords" content="{{ !empty($page->meta_keywords) ? $page->meta_keywords : '' }}">
 @endsection
 
 @section('content')
