0c7ce0785a3c5fc64fd521de0bf4aa9a75af36b1
Andrej Kopp <admin@sequel.one>
2023-05-07 03:27:51 +0200

    Added support Laravel 10

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -14,9 +14,9 @@
                     'class'         => 'card bg-success text-white',
                     'content'    => [
                         'body'   => '
-                            <strong>Version</strong>: <a class="text-white" href="https://github.com/SequelONE/laravel-auth-backpack">v2.0.0</a><br />
+                            <strong>Version</strong>: <a class="text-white" href="https://github.com/SequelONE/laravel-auth-backpack">v3.0.0</a><br />
                             <strong>Website</strong>: <a class="text-white" href="https://sequel.one">sequel.one</a><br />
-                            <strong>Docs</strong>: <a class="text-white" href="https://docs.sequel.one/v2.0/laravel-auth-backpack">docs.sequel.one</a><br />
+                            <strong>Docs</strong>: <a class="text-white" href="https://docs.sequel.one/v3.0/laravel-auth-backpack">docs.sequel.one</a><br />
                             <strong>Blog</strong>: <a class="text-white" href="https://blog.sequel.one">blog.sequel.one</a>
                         ',
                     ]
