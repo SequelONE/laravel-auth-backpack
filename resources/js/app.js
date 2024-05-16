f7dd5918f30f68e20ce295b4e8917c449395cc28
Andrej Kopp <andrej.kopp@ipaxx.com>
2024-05-16 10:55:01 +0200

    Fixed Bootstrap 5 template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,2 @@
 import './bootstrap';
-
-import 'moment/min/moment.min';
-import 'ionicons';
-import 'remixicon/fonts/remixicon.css';
-import 'wnumb/wNumb.min';
-import 'chart.js/auto';
-import 'dropzone/dist/dropzone-min';
-import 'node-snackbar';
-
-import './scripts';
-
 import '../sass/app.scss';
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,8 +3,8 @@
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-import jQuery from "jquery";
-window.$ = window.jQuery = jQuery;
+import $ from 'jquery';
+window.jQuery = window.$ = $;
 
 import * as Popper from '@popperjs/core';
 window.Popper = Popper;
